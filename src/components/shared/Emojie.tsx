a60086a8057577867c22ccc8850bee92660b1958
mahshid-sl <mahshiid.soleiman@gmail.com>
2025-08-17 19:28:51 +0330

    Add EmojiPicker component for emoji selection functionality

---
+++ src/components/shared/Emojie.tsx
@@ -0,0 +1,11 @@
+import EmojiPicker from 'emoji-picker-react';
+
+function Emojie() {
+  return (
+    <div className="">
+      <EmojiPicker />
+    </div>
+  );
+}
+
+export default Emojie;
--- src/features/chat/components/chatwindow/ChatInput.tsx
+++ src/features/chat/components/chatwindow/ChatInput.tsx
@@ -2,30 +2,51 @@ import { useState } from 'react';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { Smile, Paperclip, Send } from 'lucide-react';
+import EmojiPicker from 'emoji-picker-react';
+import type { EmojiClickData } from 'emoji-picker-react';
 
 type ChatInputProps = {
   onSend: (message: string) => void;
 };
 
 export default function ChatInput({ onSend }: ChatInputProps) {
-  const [text, setText] = useState('');
+  const [message, setMessage] = useState('');
+  const [showEmoji, setShowEmoji] = useState(false);
+
+  const onEmojiClick = (emojiObject: EmojiClickData) => {
+    setMessage((prev) => prev + emojiObject.emoji);
+  };
 
   const handleSend = () => {
-    if (!text.trim()) return;
-    onSend(text);
-    setText('');
+    if (!message.trim()) return;
+    onSend(message);
+    setMessage('');
   };
 
   return (
     <div className="flex-shrink-0 border-t bg-white p-4">
       <div className="flex items-center gap-4 rounded-lg bg-white p-2">
-        <Button variant="ghost" size="icon">
-          <Smile className="h-6 w-6" />
-        </Button>
+        {/* Emoji Picker */}
+        {
+          <Button
+            onClick={() => setShowEmoji((prev) => !prev)}
+            variant="ghost"
+            size="icon"
+          >
+            <Smile className="h-6 w-6" />
+          </Button>
+        }
+
+        {showEmoji && (
+          <div className="absolute bottom-12 left-0 z-50">
+            <EmojiPicker onEmojiClick={onEmojiClick} />
+          </div>
+        )}
+
         <Input
           type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
           placeholder="Type a message..."
           onKeyDown={(e) => e.key === 'Enter' && handleSend()}
           className="text-dark border-none bg-gray-100 placeholder:text-xs focus-visible:ring-0 focus-visible:ring-offset-0 md:placeholder:text-sm lg:placeholder:text-base"
